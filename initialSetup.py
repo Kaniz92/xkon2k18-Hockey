86a640e9503ec4d3dd009f90b91d7d8cb3babd33
Kaniz92 <kanishkasilva22@gmail.com>
2019-03-30 10:47:18 +0530

    init

---
+++ initialSetup.py
@@ -0,0 +1,13 @@
+import gym_air_hockey
+import gym
+import pygame
+
+if __name__ == "__main__":
+    env = gym.make('AirHockey-v0')
+    while True:
+        if any([event.type == pygame.QUIT for event in pygame.event.get()]): break
+        env.step()
+    pygame.quit ()
+
+#if __name__ == "__main__":
+      #  processor = gym_air_hockey.DataProcessor()
